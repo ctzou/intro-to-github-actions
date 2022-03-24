c76d1351553438e089c3bb656c6d9b52c43f5373
ctzou <6112684+ctzou@users.noreply.github.com>
2022-03-24 05:04:31 -0700

    hellooooo

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          O v O
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
@@ -16,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          hellooooo
         </a>
       </header>
     </div>
